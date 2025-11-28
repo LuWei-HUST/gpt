dcfa2fce43031417b27815b05da5fdf4a5d3bcea
Lu Wei <1261579359@qq.com>
2025-11-28 20:12:09 +0800

    init

---
+++ SimpleTokenizerV1.py
@@ -0,0 +1,66 @@
+import re
+
+
+class SimpleTokenizerV1:
+
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+
+        return ids
+    
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+
+        return text
+    
+
+raw_text = ""
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+
+# print(len(vocab.items()))
+
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+
+tokenizer = SimpleTokenizerV1(vocab)
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+# print(text)
+
+tokenizer = SimpleTokenizerV1(vocab)
+# print(tokenizer.encode(text))
+
+# print(tokenizer.decode(tokenizer.encode(text)))
+
+from importlib.metadata import version
+import tiktoken
+print("tiktoken version:", version("tiktoken"))
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+     "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
